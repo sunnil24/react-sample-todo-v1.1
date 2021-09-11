4659b7e931a1d11babec4369e5c58ac09600d780
sunnil24 <sunilsri.srivastava3@gmail.com>
2021-09-11 19:30:49 +0530

    1 file modified

--- src/Components/ToDoList/ToDoList.jsx
+++ src/Components/ToDoList/ToDoList.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useCallback } from 'react';
+import React, { useEffect, useCallback, useState } from 'react';
 import useStore from '../../hooks/useStore';
 import { fetchToDos, updateToDos } from '../../store/actions';
 import './style.css';
@@ -7,6 +7,7 @@ import ToDoItem from '../ToDoItem';
 
 export default function ToDoList() {
   const [state, dispatch] = useStore();
+  const [page, setPage] = useState(1);
   const { todos } = state;
 
   useEffect(() => {
@@ -38,21 +39,27 @@ export default function ToDoList() {
   );
 
   return (
-    <ol className="todo-list">
-      {todos
-        ?.sort((todo1, todo2) => todo2.id - todo1.id)
-        ?.map(item => (
-          <li
-            key={`${item.userId}-${item.title}`}
-            className={item.completed ? 'completed' : ''}
-          >
-            <ToDoItem
-              item={item}
-              toggleHandler={toggleToDo}
-              deleteHandler={deleteToDo}
-            />
-          </li>
-        ))}
-    </ol>
+    <>
+      <ol className="todo-list">
+        {todos
+          ?.sort((todo1, todo2) => todo2.id - todo1.id)
+          .filter((item, index) => {
+            return index < page * 10 && item;
+          })
+          .map(item => (
+            <li
+              key={`${item.userId}-${item.title}`}
+              className={item.completed ? 'completed' : ''}
+            >
+              <ToDoItem
+                item={item}
+                toggleHandler={toggleToDo}
+                deleteHandler={deleteToDo}
+              />
+            </li>
+          ))}
+      </ol>
+      <button type="button">Load More</button>
+    </>
   );
 }
